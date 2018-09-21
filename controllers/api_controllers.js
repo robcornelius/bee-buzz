9e626309801388bf169a8e0588b468c75515ece8
Rob Cornelius <rob.cornelius@oracle.com>
2018-09-21 16:50:21 +0100

    mongoose

--- controllers/api_controllers.js
+++ controllers/api_controllers.js
@@ -1,6 +1,16 @@
 "use strict";
 
 var express = require('express');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/bees');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+
+});
 
 const postHive = (id, data) =>  {
 
